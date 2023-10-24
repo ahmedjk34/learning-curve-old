579f5ee15170614655d3e5db22dfcc5d9ce8cbc8
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-10-24 21:23:31 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import "./styles/pages/global-rules.scss";
+type Props = {};
+
+function App({}: Props) {
+  return <div>App</div>;
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,43 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const httpErrors = require("http-errors");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(httpErrors(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
