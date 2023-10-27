6661555826ef58009c445b7dedb443e7e054f7b5
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-10-27 22:04:36 +0200

    edited routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import CoursePage from "./components/course-page/CoursePage";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import SearchPage from "./components/SearchPage";
 import "./styles/pages/global-rules.scss";
 type Props = {};
 
 function App({}: Props) {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/course/:id" element={<CoursePage />}></Route>
+        <Route path="/search" element={<SearchPage />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
