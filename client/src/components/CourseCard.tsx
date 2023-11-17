ccf201bb74e08caa72973c8df43b1318fe5a9fa5
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-11-17 20:13:15 +0200

    Added auth0 to the card , added navigation

--- client/src/components/CourseCard.tsx
+++ client/src/components/CourseCard.tsx
@@ -1,12 +1,58 @@
 import styles from "../styles/pages/main.module.scss";
 import { Rating } from "react-simple-star-rating";
-import { Course } from "../Types";
+import { Course, CustomUser } from "../Types";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-type Props = {
+type CourseProps = {
   course: Course;
 };
+type ButtonProps = {
+  courseId: String;
+};
+function EnrollButton({ courseId }: ButtonProps) {
+  const [isEnrolled, setIsEnrolled] = useState(false);
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
+  const navigation = useNavigate();
+  if (!isAuthenticated)
+    return <button onClick={() => loginWithRedirect()}>Enroll Now</button>;
+  const userId = user?.sub?.substring(user?.sub?.indexOf("|") + 1);
+  axios.get(`http://localhost:3000/user/${userId}`).then((response) => {
+    const userData = response.data[0] as CustomUser;
+    userData.episodesWatched.forEach((course) => {
+      if (courseId == course.courseId) {
+        setIsEnrolled(true);
+      }
+    });
+  });
+  if (isEnrolled) {
+    return (
+      <button
+        style={{ backgroundColor: "green" }}
+        onClick={() => navigation(`/course/${courseId}`)}
+      >
+        Continue Watching
+      </button>
+    );
+  }
+  return (
+    <button
+      onClick={() => {
+        axios.post(`http://localhost:3000/user/course/${courseId}`, {
+          userId: userId,
+          courseId: courseId,
+        });
+        navigation(`/course/${courseId}`);
+      }}
+    >
+      Enroll Now
+    </button>
+  );
+}
 
-function CourseCard({ course }: Props) {
+function CourseCard({ course }: CourseProps) {
   return (
     <div key={course.title} className={styles.course}>
       <img
@@ -24,7 +70,7 @@ function CourseCard({ course }: Props) {
         <h4>{course.enrolledStudents} Students Enrolled</h4>
       </div>
       <h3>{course.title}</h3>
-      <button>Enroll Now</button>
+      <EnrollButton courseId={course._id}></EnrollButton>
     </div>
   );
 }
