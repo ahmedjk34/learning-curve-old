7ef38c5e4fa9426fe2193371baf5194f3e093048
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-11-17 15:52:51 +0200

    Setting up auth0

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,7 +1,10 @@
 import styles from "../styles/pages/nav.module.scss";
 import logo from "../assets/logo.png";
-import pfp from "../assets/personal.png";
+
 import { AiOutlineMenu } from "react-icons/ai";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import axios from "axios";
 type Props = {};
 
 const groupStyle = {
@@ -12,6 +15,20 @@ const groupStyle = {
 };
 
 function Nav({}: Props) {
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
+    useAuth0();
+
+  useEffect(() => {
+    //in case of login out
+    if (!isAuthenticated) return;
+    //when logging in
+    const userId = user?.sub?.substring(user?.sub?.indexOf("|") + 1);
+    console.log(userId);
+    axios.post(`http://localhost:3000/user/${userId}`, {
+      userId: userId,
+      episodesWatched: [],
+    });
+  }, [isAuthenticated]);
   return (
     <div className={styles.nav}>
       <img src={logo} className={styles.logo} alt="Logo"></img>
@@ -21,9 +38,35 @@ function Nav({}: Props) {
           <h3>Courses</h3>
           <h3>About</h3>
         </div>
+
         <div className={styles.profileMenu}>
-          <AiOutlineMenu color="white" className={styles.icon} size="20" />
-          <img src={pfp} alt="Profile Picture"></img>
+          {!isLoading &&
+            (isAuthenticated ? (
+              <>
+                <AiOutlineMenu
+                  color="white"
+                  className={styles.icon}
+                  size="20"
+                />
+                <img src={user?.picture} alt="Profile Picture"></img>
+                <div className={styles.dropdown}>
+                  <h4>Courses</h4>
+                  <h4
+                    onClick={() =>
+                      logout({
+                        logoutParams: { returnTo: window.location.origin },
+                      })
+                    }
+                  >
+                    Log out
+                  </h4>
+                </div>
+              </>
+            ) : (
+              <div>
+                <button onClick={() => loginWithRedirect()}>Login</button>
+              </div>
+            ))}
         </div>
       </div>
     </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <App />
+    <Auth0Provider
+      domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`}
+      clientId={`${process.env.REACT_APP_AUTH0_CLIENT_ID}`}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </HashRouter>
 );
