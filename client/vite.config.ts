ace10fa0c9af44c725799ba6055789d33ae04a03
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-11-17 15:52:31 +0200

    Allowed .env files in Vite , & converted to https

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react(), basicSsl()],
+  };
 });
